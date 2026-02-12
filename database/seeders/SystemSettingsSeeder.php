ba90346bb03aca398f43885a4c76a909156ddbaf
Abdelrhman Mgahed <41325933+Mgahed@users.noreply.github.com>
2026-02-12 23:15:05 +0200

    Update SystemSettingsSeeder with new company details

--- database/seeders/SystemSettingsSeeder.php
+++ database/seeders/SystemSettingsSeeder.php
@@ -13,17 +13,17 @@ class SystemSettingsSeeder extends Seeder
     public function run(): void
     {
         SystemSetting::firstOrCreate([], [
-            'company_name' => 'Darezini Naturdarme',
-            'general_manager' => 'Darezini Mohamad',
-            'health_approval_number' => 'DE NW20176 EG',
-            'full_address' => 'Industrie Str. 23, 32825 Blomberg, Deutschland',
-            'landline' => '+4952354750547',
-            'mobile' => '+491789208986',
+            'company_name' => 'Mgahed',
+            'general_manager' => 'Abdelrhman Mgahed',
+            'health_approval_number' => 'EG',
+            'full_address' => 'Egypt',
+            'landline' => '+201228954237',
+            'mobile' => '+201228954237',
             'whatsapp_enabled' => true,
-            'website' => 'DAREZINI.COM',
-            'tax_id' => '313/5064/3253',
-            'vat_no' => 'DE 325100294',
-            'eori_no' => 'DE361415158134250',
+            'website' => 'https://mgahed.com',
+            'tax_id' => '3',
+            'vat_no' => 'EG',
+            'eori_no' => 'EG',
         ]);
     }
 }
